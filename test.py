03d236dec81b321e17803e2d6d7fde2dbbd1de4a
Eric-Acha <eric.techcore1@gmail.com>
2025-03-29 22:11:28 -0400

    This is the first try. Still working...

---
+++ test.py
@@ -0,0 +1,16 @@
+# Test if old enough to drive
+
+def can_drive():
+    name = input("enter name: ")
+    age = int(input("enter age: "))
+    license_satus = True
+
+    if age > 18 and license_satus:
+        print(f"Welcome {name}, you are good to go!.")
+    elif age == 18 and license_satus:
+        print(f"Hello {name}, you can drive with a qulified adult.")
+    else:
+        print(f"{name}, you are not qualified to drive.")
+can_drive() 
+
+
