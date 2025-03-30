780f7a553b30cb4264387b6bd0613458f07985f2
Eric-Acha <eric.techcore1@gmail.com>
2025-03-30 00:18:10 -0400

    set of python functions

---
+++ calc.py
@@ -0,0 +1,25 @@
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def addition(num1, num2):
+    return num1 + num2
+
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+
+def division(num1, num2):
+    return num1/num2
+
+
+def square(num):
+    return num * num
+
+
+print(square(10))
+print(subtraction(13, 3))
+print(addition(10, 60))
+print(division(100, 10))
+print(square(multiply(2, 5)))
