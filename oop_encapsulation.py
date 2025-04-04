e66ee104fb6893dfef7d6ce08b5966a9e46bba55
Eric-Acha <eric.techcore1@gmail.com>
2025-04-04 02:22:52 -0400

    Encapsulating the obj with a greetings method

---
+++ oop_encapsulation.py
@@ -0,0 +1,17 @@
+# Encapsulation
+
+class PlayerCharacter:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # I can add a method to encapsulate this object
+
+    def greetings(self):
+        print(f"Hello {self.name}, you are {self.age} years old.")
+        return 'done'
+
+
+player1 = PlayerCharacter('John', 35)
+
+print(player1.greetings())
