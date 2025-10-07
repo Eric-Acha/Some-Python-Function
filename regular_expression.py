4d0aa66893832a25af4d8365261bb7de4973f5e0
Eric-Acha <eric.techcore1@gmail.com>
2025-10-06 22:45:22 -0400

    Password validation regular expression

--- regular_expression.py
+++ regular_expression.py
@@ -2,9 +2,13 @@ import re
 
 pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
 string = 'eric2233+-@123gstwffff.com'
+pattern2 = re.compile(r"[A-Za-z0-9$%#@]{8,\d}")
+
+password = 'jaw43%$8'
 
 
 a = pattern.search(string)
+check = pattern2.fullmatch(password)
 
 
-print(a)
+print(check)
