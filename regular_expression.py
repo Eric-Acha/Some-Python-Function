e51710d72e17181edbb2f92890fa96faa6fa2886
Eric-Acha <eric.techcore1@gmail.com>
2025-10-06 22:18:08 -0400

    More regular expression

--- regular_expression.py
+++ regular_expression.py
@@ -1,8 +1,12 @@
 import re
 
+pattern = re.compile(r"([a-zA-Z]).([a])")
 string = 'search inside this text please!'
 
-a = re.search('this', string)
-print(a.span())
-print(a.start())
-print(a.group())
+
+a = pattern.search(string)
+b = pattern.findall(string)
+c = pattern.fullmatch(string)
+d = pattern.match(string)
+
+print(a.group(2))
