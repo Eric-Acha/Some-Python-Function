b3575d4c4765ff6d40f35138e44de010e4cf5731
Eric-Acha <eric.techcore1@gmail.com>
2025-10-10 22:51:32 -0400

    Having fun with the thumbnail method

--- image-folder/image.py
+++ image-folder/image.py
@@ -1,9 +1,18 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('snail.jpg')
+img = Image.open('profile3.png')
+
+img.thumbnail((50, 50))
+
+img.save('profile4.png')
 
 
-box = (100, 100, 300, 300)
-region = img.crop(box)
+# img.filter(ImageFilter.SMOOTH)
 
-region.save("lion.png", 'png')
+# img = img.save('profile3.png', 'png')
+
+
+# box = (100, 100, 300, 300)
+# region = img.crop(box)
+
+# region.save("profile.png", 'png')
